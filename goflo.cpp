c285daaadac2c5ec591ed78c372c41b6e88c8bc0
Kyle Cesare <kcesare@gmail.com>
2014-11-11 15:57:32 -0800

    Dense flow.

--- goflo.cpp
+++ goflo.cpp
@@ -18,10 +18,6 @@ int main(int argc, char **argv) {
   capture >> prevFrame;
   cv::cvtColor(prevFrame, prevFrame, cv::COLOR_BGR2GRAY);
 
-  // Find interesting features
-  std::vector<cv::Point2f> originalPoints;
-  cv::goodFeaturesToTrack(prevFrame, originalPoints, 500, 0.01, 1.0);
-
   while(true) {
     // Grab the current frame
     cv::Mat curFrame;
@@ -33,31 +29,16 @@ int main(int argc, char **argv) {
     // Convert the frame to grayscale
     cv::cvtColor(curFrame, curFrame, cv::COLOR_BGR2GRAY);
 
-    // Find the interesting points in the current frame
-    std::vector<cv::Point2f> newPoints;
-    cv::goodFeaturesToTrack(curFrame, newPoints, 100, 0.3, 7.0);
+    cv::Mat flow;
+    cv::calcOpticalFlowFarneback(prevFrame, curFrame, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
 
-    // Draw the points to the debug frame
-    for(auto k : originalPoints) {
-      cv::circle(drawnFrame, k, 3, cv::Scalar(0, 0, 255));
-    }
+    for(int y = 0; y < flow.rows; y += 10) {
+      for(int x = 0; x < flow.cols; x += 10) {
+        const cv::Point2f& dxdy = flow.at<cv::Point2f>(y, x);
 
-    // Calculate the optical flow using Lucas-Kanade method
-    std::vector<unsigned char> statuses;
-    std::vector<float> errors;
-    cv::calcOpticalFlowPyrLK(prevFrame, curFrame, originalPoints, newPoints,
-        statuses, errors, cv::Size(15, 15), 2,
-        cv::TermCriteria(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 10, 0.03));
-
-    // Draw arrows from all of the original points to their new locations
-    for(int i = 0; i < newPoints.size(); i++) {
-      // std::cout << "Error is " << errors[i] << std::endl;
-
-      if(statuses[i] == 1) {
-        cv::Point from(std::round(originalPoints[i].x), std::round(originalPoints[i].y));
-        cv::Point to(std::round(newPoints[i].x), std::round(newPoints[i].y));
-
-        cv::line(drawnFrame, from, to, cv::Scalar(255, 0, 0));
+        int sumSquare = std::pow(dxdy.x, 2) + std::pow(dxdy.y, 2);
+        // cv::circle(drawnFrame, cv::Point(x, y), std::sqrt(sumSquare) / 2, cv::Scalar(0, 0, sumSquare, 100), -1);
+        cv::line(drawnFrame, cv::Point(x, y), cv::Point(x + dxdy.x, y + dxdy.y), cv::Scalar(255, 0, 0));
       }
     }
 
@@ -66,8 +47,8 @@ int main(int argc, char **argv) {
       break;
     }
 
-    // // Current frame is now the previous frame
-    // prevFrame = curFrame;
+    // Current frame is now the previous frame
+    prevFrame = curFrame;
   }
 
   return EXIT_SUCCESS;
