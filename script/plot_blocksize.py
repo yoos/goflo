914348afc5ac48a602ce1266cd29e439705e00a4
Kyle Cesare <kcesare@gmail.com>
2014-12-05 19:21:13 -0800

    Split resoultion and block size scripts.

--- script/plot_blocksize.py
+++ script/plot_blocksize.py
@@ -5,13 +5,13 @@ plt.style.use('ggplot')
 
 ys = [[550, 600, 65], [500, 700, 90], [700, 800, 900]]
 
-plt.boxplot(ys, labels=['320x240', '640x480', '1920x1080'])
+plt.boxplot(ys, labels=['9', '13', '17'])
 
-plt.title("Resolution vs. Processing Time")
+plt.title("Block Size vs. Processing Time")
 plt.ylim((0, 1000))
-plt.xlabel("Resolution")
+plt.xlabel("Block Size")
 plt.ylabel("Time (ms)")
 
 fig = plt.gcf()
 fig.set_size_inches(8, 6)
-fig.savefig('resolution.png', dpi=100)
+fig.savefig('block_size.png', dpi=100)
