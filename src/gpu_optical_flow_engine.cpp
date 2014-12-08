442be7949d11bf1aab05ad1eb9f9f9d8c2e81a48
Kyle Cesare <kcesare@gmail.com>
2014-12-07 17:54:10 -0800

    Revert "Try again."
    This reverts commit 19c80aada378ed4a5f3548cc9bcec5340981a95e.

--- src/gpu_optical_flow_engine.cpp
+++ src/gpu_optical_flow_engine.cpp
@@ -1,18 +1,18 @@
 #include "gpu_optical_flow_engine.hpp"
 
 std::vector<cv::Mat> GPUOpticalFlowEngine::process(cv::Mat& prevFrame, cv::Mat& nextFrame) {
-  cv::ocl::oclMat gpuPrevFrame(prevFrame);
-  cv::ocl::oclMat gpuNextFrame(nextFrame);
+  cv::gpu::GpuMat gpuPrevFrame(prevFrame);
+  cv::gpu::GpuMat gpuNextFrame(nextFrame);
 
-  cv::ocl::oclMat gpuFlowX;
-  cv::ocl::oclMat gpuFlowY;
+  cv::gpu::GpuMat gpuFlowX;
+  cv::gpu::GpuMat gpuFlowY;
   
-  // opticalFlow(gpuPrevFrame, gpuNextFrame, gpuFlowX, gpuFlowY);
+  opticalFlow(gpuPrevFrame, gpuNextFrame, gpuFlowX, gpuFlowY);
   cv::calcOpticalFlowFarneback(gpuPrevFrame, gpuNextFrame, gpuFlowX, 0.5, 5, 13, 10, 5, 1.1, 0);
 
   std::vector<cv::Mat> flow(2);
   gpuFlowX.download(flow[0]); // TODO(kyle): Use both X and Y directions
-  // gpuFlowY.download(flow[1]);
+  gpuFlowY.download(flow[1]);
 
   return flow;
 }
--- src/gpu_optical_flow_engine.hpp
+++ src/gpu_optical_flow_engine.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/ocl/ocl.hpp>
+#include <opencv2/gpu/gpu.hpp>
 
 #include "optical_flow_engine.hpp"
 
 class GPUOpticalFlowEngine : public OpticalFlowEngine {
 public:
   virtual std::vector<cv::Mat> process(cv::Mat& prevFrame, cv::Mat& nextFrame);
+
+private:
+  cv::gpu::FarnebackOpticalFlow opticalFlow;
 };
