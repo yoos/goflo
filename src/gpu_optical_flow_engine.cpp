b311d0011572a3584c3d0ad275fea3de1f727eee
Kyle Cesare <kcesare@gmail.com>
2014-12-07 17:54:20 -0800

    Revert "Try to call GPU optical flow just like CPU."
    This reverts commit 5b84b1a068b22556658e0e98a2b987cc4be682a5.

--- src/gpu_optical_flow_engine.cpp
+++ src/gpu_optical_flow_engine.cpp
@@ -8,7 +8,6 @@ std::vector<cv::Mat> GPUOpticalFlowEngine::process(cv::Mat& prevFrame, cv::Mat& 
   cv::gpu::GpuMat gpuFlowY;
   
   opticalFlow(gpuPrevFrame, gpuNextFrame, gpuFlowX, gpuFlowY);
-  cv::calcOpticalFlowFarneback(gpuPrevFrame, gpuNextFrame, gpuFlowX, 0.5, 5, 13, 10, 5, 1.1, 0);
 
   std::vector<cv::Mat> flow(2);
   gpuFlowX.download(flow[0]); // TODO(kyle): Use both X and Y directions
